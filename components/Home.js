dd580db561ac1efdc0e344488fcf457cf1015800
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-02-09 22:01:57 +0530

    fix loader

--- components/Home.js
+++ components/Home.js
@@ -21,32 +21,7 @@ const Home = ({ posts, menus }) => {
   return (
     <>
     <Header menus={menus}/>
-      <div className="loading-container">
-        <div className="h-100 d-flex align-items-center justify-content-center">
-          <ul className="list-unstyled">
-            <li>
-              <img
-                src="images/placeholder/loading.png"
-                alt="Alternate Text"
-                height="100"
-              />
-            </li>
-            <li>
-              <div className="spinner">
-                <div className="rect1"></div>
-                <div className="rect2"></div>
-                <div className="rect3"></div>
-                <div className="rect4"></div>
-                <div className="rect5"></div>
-              </div>
-            </li>
-            <li>
-              <p>Loading</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-
+    
       {/* <!-- Tranding news  carousel-->*/}
       <section className="bg-light pt-20 carouselContainer">
         <div className="container">
