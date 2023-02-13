ccb0eaed4c2675d5878425ff489e4312a8cfbb7c
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-02-13 23:15:12 +0530

    Search page update by data

--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -1,8 +1,15 @@
 import React from "react";
 import { getAllPosts } from "../../utils/wpGraph";
+import { useSearchParams } from 'next/navigation'
+import Image from "next/image";
+import Link from "next/link";
 
 const search = ({posts}) => {
   console.log('posts: ', posts);
+  const searchParams = useSearchParams()
+  const data = searchParams.get('search')
+
+  const searchData = posts?.nodes
 
   return (
     <>
@@ -13,208 +20,69 @@ const search = ({posts}) => {
               <div className="wrap__search-result">
                 <div className="wrap__search-result-keyword">
                   <h2>
-                    Search results for keyword:
-                    <span className="text-primary">"corona virus"</span>
-                    found in 4 posts.
+                    Search Results For You :
+                    <span className="text-primary"> {data}</span>
                   </h2>
                 </div>
               </div>
+              
               {/* <!-- Post Article List --> */}
-              <div className="card__post card__post-list card__post__transition mt-30">
+          {  searchData.map((items)=>{
+            return(
+               <div className="card__post card__post-list card__post__transition mt-30">
                 <div className="row ">
                   <div className="col-md-5">
                     <div className="card__post__transition">
-                      <a href="/">
-                        <img
-                          src="images/placeholder/500x400.jpg"
+                      <Link href={`/posts/${items.slug}`}>
+                        <Image
+                          src={items.featuredImage?.node.sourceUrl}
+                          width={500}
+                          height={400}
                           className="img-fluid w-100"
-                          alt=""
+                          alt={items.featuredImage?.node.altText}
                         />
-                      </a>
+                      </Link>
                     </div>
                   </div>
                   <div className="col-md-7 my-auto pl-0">
                     <div className="card__post__body ">
                       <div className="card__post__content  ">
-                        <div className="card__post__category ">travel</div>
+                        <div className="card__post__category ">{items?.categories?.nodes[0]?.name}</div>
                         <div className="card__post__author-info mb-2">
                           <ul className="list-inline">
                             <li className="list-inline-item">
                               <span className="text-primary">
-                                by david hall
+                                    {items.author.node.name}
                               </span>
                             </li>
                             <li className="list-inline-item">
                               <span className="text-dark text-capitalize">
-                                descember 09, 2016
+                                    {items.date}
                               </span>
                             </li>
                           </ul>
                         </div>
                         <div className="card__post__title">
                           <h5>
-                            <a href="/">
-                              Exercitation Ullamco Laboris Nisi Ut Aliquip
-                            </a>
+                                <Link href={`/posts/${items.slug}`}>
+                                  {items.title.slice(0,40)}
+                            </Link>
                           </h5>
-                          <p className="d-none d-lg-block d-xl-block mb-0">
+                          {/* <p className="d-none d-lg-block d-xl-block mb-0">
                             Maecenas accumsan tortor ut velit pharetra mollis.
                             Proin eu nisl et arcu iaculis placerat sollicitudin
                             ut est. In fringilla dui dui.
-                          </p>
+                          </p> */}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              {/* <!-- Post Article List --> */}
-              <div className="card__post card__post-list card__post__transition mt-30">
-                <div className="row ">
-                  <div className="col-md-5">
-                    <div className="card__post__transition">
-                      <a href="/">
-                        <img
-                          src="images/placeholder/500x400.jpg"
-                          className="img-fluid w-100"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-md-7 my-auto pl-0">
-                    <div className="card__post__body ">
-                      <div className="card__post__content  ">
-                        <div className="card__post__category ">travel</div>
-                        <div className="card__post__author-info mb-2">
-                          <ul className="list-inline">
-                            <li className="list-inline-item">
-                              <span className="text-primary">
-                                by david hall
-                              </span>
-                            </li>
-                            <li className="list-inline-item">
-                              <span className="text-dark text-capitalize">
-                                descember 09, 2016
-                              </span>
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="card__post__title">
-                          <h5>
-                            <a href="/">
-                              Exercitation Ullamco Laboris Nisi Ut Aliquip
-                            </a>
-                          </h5>
-                          <p className="d-none d-lg-block d-xl-block mb-0">
-                            Maecenas accumsan tortor ut velit pharetra mollis.
-                            Proin eu nisl et arcu iaculis placerat sollicitudin
-                            ut est. In fringilla dui dui.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <!-- Post Article List --> */}
-              <div className="card__post card__post-list card__post__transition mt-30">
-                <div className="row ">
-                  <div className="col-md-5">
-                    <div className="card__post__transition">
-                      <a href="/">
-                        <img
-                          src="images/placeholder/500x400.jpg"
-                          className="img-fluid w-100"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-md-7 my-auto pl-0">
-                    <div className="card__post__body ">
-                      <div className="card__post__content  ">
-                        <div className="card__post__category ">travel</div>
-                        <div className="card__post__author-info mb-2">
-                          <ul className="list-inline">
-                            <li className="list-inline-item">
-                              <span className="text-primary">
-                                by david hall
-                              </span>
-                            </li>
-                            <li className="list-inline-item">
-                              <span className="text-dark text-capitalize">
-                                descember 09, 2016
-                              </span>
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="card__post__title">
-                          <h5>
-                            <a href="/">
-                              Exercitation Ullamco Laboris Nisi Ut Aliquip
-                            </a>
-                          </h5>
-                          <p className="d-none d-lg-block d-xl-block mb-0">
-                            Maecenas accumsan tortor ut velit pharetra mollis.
-                            Proin eu nisl et arcu iaculis placerat sollicitudin
-                            ut est. In fringilla dui dui.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <!-- Post Article List --> */}
-              <div className="card__post card__post-list card__post__transition mt-30">
-                <div className="row ">
-                  <div className="col-md-5">
-                    <div className="card__post__transition">
-                      <a href="/">
-                        <img
-                          src="images/placeholder/500x400.jpg"
-                          className="img-fluid w-100"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-md-7 my-auto pl-0">
-                    <div className="card__post__body ">
-                      <div className="card__post__content  ">
-                        <div className="card__post__category ">travel</div>
-                        <div className="card__post__author-info mb-2">
-                          <ul className="list-inline">
-                            <li className="list-inline-item">
-                              <span className="text-primary">
-                                by david hall
-                              </span>
-                            </li>
-                            <li className="list-inline-item">
-                              <span className="text-dark text-capitalize">
-                                descember 09, 2016
-                              </span>
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="card__post__title">
-                          <h5>
-                            <a href="/">
-                              Exercitation Ullamco Laboris Nisi Ut Aliquip
-                            </a>
-                          </h5>
-                          <p className="d-none d-lg-block d-xl-block mb-0">
-                            Maecenas accumsan tortor ut velit pharetra mollis.
-                            Proin eu nisl et arcu iaculis placerat sollicitudin
-                            ut est. In fringilla dui dui.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            )
+          })
+              
+              }
             </div>
           </div>
         </div>
