252a3b5131e2a8652b6f92c02f1d70b6598ac6eb
prateek.singh <prateek.singh@ultragenicglobal.com>
2023-03-01 21:22:08 +0530

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-});
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+// });
 
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
-    unoptimized: true,
+    optimized: true,
     //enter the domain or subdomain where you have WordPress installed
     domains: ['i0.wp.com', 'https://12thmanstaging.the12thman.in'],
   },
   staticPageGenerationTimeout: 1000,
-});
+};
+
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   output: 'standalone',
+//   images: {
+//     unoptimized: true,
+//     //enter the domain or subdomain where you have WordPress installed
+//     domains: ['i0.wp.com', 'https://12thmanstaging.the12thman.in'],
+//   },
+//   staticPageGenerationTimeout: 1000,
+// });
 
 module.exports = nextConfig;
