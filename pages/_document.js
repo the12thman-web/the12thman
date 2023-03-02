f84874e5b4b53e14dcc09115a14f46baf9712e2b
prateek.singh <prateek.singh@ultragenicglobal.com>
2023-03-02 23:01:31 +0530

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -12,16 +12,20 @@ export default function Document() {
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891586352099803"
         />
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-       	(adsbygoogle = window.adsbygoogle || []).push({
-         	google_ad_client: "ca-pub-9891586352099803",
-         	enable_page_level_ads: true
-         	});
-        	`,
-          }}
-        /> */}
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-Q6CJDCHK19`}
+        />
+        <Script strategy="lazyOnload">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config','G-Q6CJDCHK19', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
       </Head>{' '}
       <body>
         {' '}
