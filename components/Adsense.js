b35751a69db2fdb92b6142a3196451a2a6f47f45
prateek.singh <prateek.singh@ultragenicglobal.com>
2023-03-02 22:40:07 +0530

    update add width

--- components/Adsense.js
+++ components/Adsense.js
@@ -19,7 +19,7 @@ export default function Adsense() {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: 'block', textAlign: 'center' }}
+      style={{ display: 'block', textAlign: 'center', width: '100%' }}
       data-ad-client="ca-pub-9891586352099803"
       data-ad-slot="5294261220"
       data-ad-format="fluid"
