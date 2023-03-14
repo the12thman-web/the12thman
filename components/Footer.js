7cfeab744a479281e24af5f4958a496f7bf25cc9
prateek.singh <prateek.singh@ultragenicglobal.com>
2023-03-14 16:45:03 +0530

    add footer add

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 import { useAmp } from 'next/amp';
+import iplWinImg from '../public/images/iplwin.png';
 
 export const config = { amp: 'hybrid' };
 
@@ -14,6 +16,20 @@ const Footer = () => {
         <>
           <section className="wrapper__section p-0">
             <div className="wrapper__section__components">
+              <div className="rswsuper365mobile">
+                {' '}
+                <a
+                  id="super365mobile"
+                  target="_blank"
+                  href="https://iplwin.us/PRblO"
+                >
+                  <Image
+                    src={iplWinImg}
+                    alt="IPLWIN"
+                    style={{ width: '100%' }}
+                  />
+                </a>{' '}
+              </div>
               {/* <!-- Footer --> */}
               <footer>
                 {/* <!-- Footer bottom --> */}
