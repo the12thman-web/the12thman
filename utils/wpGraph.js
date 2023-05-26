3e1ab038ecc9d7033078b10baec4c47363212e16
Eishi2012 <eishi2012@gmail.com>
2023-05-26 11:55:22 +0530

    Update wpGraph.js

--- utils/wpGraph.js
+++ utils/wpGraph.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://backend.the12thman.in/graphql';
+const API_URL = 'https://backendrsw.the12thman.in/graphql';
 
 async function fetchAPI(query = '', { variables } = {}) {
   const headers = {
