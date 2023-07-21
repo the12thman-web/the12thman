2e740f8de0b11fef698c7bfca160b57b4791db9f
AkankshaVista <akanksha.garg@vista.com>
2023-07-21 15:05:07 +0530

    Updating sidebar and header menu

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { IoSearch } from 'react-icons/io5';
+import { MobileMenuSideBar } from './MobileMenuSideBar';
 
 const Header = () => {
   // distructuring the main menu from menu object
@@ -31,15 +32,33 @@ const Header = () => {
 
   return (
     <header className="header">
-      <nav className="navbar container px-1 sm:px-8">
+      <nav className="navbar container px-1 sm:px-7">
+        <button
+          onClick={() => setShowMenu(!showMenu)}
+          className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-primary text-white lg:hidden"
+        >
+          {showMenu ? (
+            <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
+              <title>Menu Close</title>
+              <polygon
+                points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+                transform="rotate(45 10 10)"
+              />
+            </svg>
+          ) : (
+            <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
+              <title>Menu Open</title>
+              <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
+            </svg>
+          )}
+        </button>
+
         <div className="order-0">
           <Logo />
         </div>
         <div className="flex items-center space-x-4 xl:space-x-8">
           <div
-            className={`collapse-menu ${
-              !showMenu && 'translate-x-full'
-            } lg:flex lg:translate-x-0`}
+            className={`collapse-menu translate-x-full lg:flex lg:translate-x-0`}
           >
             <button
               className="absolute right-6 top-11 lg:hidden"
@@ -55,7 +74,7 @@ const Header = () => {
             </button>
             <ul
               id="nav-menu"
-              className="navbar-nav w-full group-hover:scale-100 md:w-auto md:space-x-1 lg:flex xl:space-x-2"
+              className="navbar-nav w-full group-hover:scale-100 md:w-auto md:space-x-1 lg:flex xl:space-x-2 ml-0"
             >
               {main.map((menu, i) => (
                 <React.Fragment key={`menu-${i}`}>
@@ -94,24 +113,10 @@ const Header = () => {
                                   {child.name}
                                 </Link>
                                 {child.hasChildren && (
-                                  <ul className="nav-dropdown-list relative hidden transition-all duration-300 group-hover:left-[150px] group-hover:block md:invisible md:absolute md:top-[40px] md:block md:opacity-0 md:group-hover:visible md:group-hover:opacity-100">
-                                    {child.children.map((childNext, i) => (
-                                      <li
-                                        className="nav-dropdown-item"
-                                        key={`children-${i}`}
-                                      >
-                                        <Link
-                                          href={childNext.url}
-                                          className={`nav-dropdown-link block ${
-                                            router.asPath === childNext.url &&
-                                            'active'
-                                          }`}
-                                        >
-                                          {childNext.name}
-                                        </Link>
-                                      </li>
-                                    ))}
-                                  </ul>
+                                  <InnerLevelMenu
+                                    parentArray={child}
+                                    router={router}
+                                  ></InnerLevelMenu>
                                 )}
                               </li>
                             ))}
@@ -146,25 +151,6 @@ const Header = () => {
           >
             <IoSearch />
           </div>
-          <button
-            onClick={() => setShowMenu(!showMenu)}
-            className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-primary text-white lg:hidden"
-          >
-            {showMenu ? (
-              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                <title>Menu Close</title>
-                <polygon
-                  points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
-                  transform="rotate(45 10 10)"
-                />
-              </svg>
-            ) : (
-              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                <title>Menu Open</title>
-                <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
-              </svg>
-            )}
-          </button>
         </div>
 
         <SearchModal
@@ -172,10 +158,40 @@ const Header = () => {
           setSearchModal={setSearchModal}
         />
       </nav>
-      {showMenu && (
+      {/* {showMenu && (
         <div className="header-backdrop absolute left-0 top-0 h-[100vh] w-full bg-black/50 lg:hidden"></div>
-      )}
+      )} */}
+
+      {/* <!-- MOBILE sidebar menu  --> */}
+      <div className="lg:hidden">
+        <MobileMenuSideBar showMenu={showMenu}></MobileMenuSideBar>
+      </div>
     </header>
+  );
+};
+
+const InnerLevelMenu = ({ parentArray, router }) => {
+  return (
+    <ul className="nav-dropdown-list relative hidden transition-all duration-300 group-hover:left-[150px] group-hover:block md:invisible md:absolute md:top-[40px] md:block md:opacity-0 md:group-hover:visible md:group-hover:opacity-100">
+      {parentArray.children.map((lastChild, i) => (
+        <li className="nav-dropdown-item" key={`children-${i}`}>
+          <Link
+            href={lastChild.url}
+            className={`nav-dropdown-link block ${
+              router.asPath === lastChild.url && 'active'
+            }`}
+          >
+            {lastChild.name}
+          </Link>
+          {lastChild.hasChildren && (
+            <InnerLevelMenu
+              parentArray={lastChild}
+              router={router}
+            ></InnerLevelMenu>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 };
 
