6475bd409ce2b96bc99b7b64a3b49eda95c77c80
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-07-23 00:56:07 +0530

    move queries to constant file

---
+++ constant/query.js
@@ -0,0 +1,184 @@
+export const GET_ALL_POST_QUERY = ` query AllPosts($category: String!,$search: String!,$tag: String!, $limit:Int!) {
+            posts(where: {status: PUBLISH, orderby: {field: DATE, order: DESC},categoryName: $category, search:$search,tag:$tag},first: $limit) {
+                nodes {
+                author {
+                    node {
+                    name
+                    }
+                }
+                categories {
+                    nodes {
+                    name
+                    }
+                }
+                featuredImage {
+                    node {
+                    altText
+                    sourceUrl
+                    }
+                }
+                postId
+                slug
+                title
+                date
+                tags {
+                    nodes {
+                    name
+                    }
+                }
+                seo {
+                    opengraphUrl
+                    opengraphTitle
+                    opengraphDescription
+                    canonical
+                    opengraphImage {
+                      uri
+                    }
+                  }
+                }
+            }
+        }`
+
+
+export const GET_ALL_POST_WITH_CONTENT_QUERY = `
+        query AllPosts($category: String!,$search: String!,,$tag: String!, $limit:Int!) {
+            posts(where: {status: PUBLISH, orderby: {field: DATE, order: DESC},categoryName: $category, search:$search,tag:$tag},first: $limit) {
+                nodes {
+                author {
+                    node {
+                    name
+                    }
+                }
+                categories {
+                    nodes {
+                    name
+                    }
+                }
+                featuredImage {
+                    node {
+                    altText
+                    sourceUrl
+                    }
+                }
+                postId
+                slug
+                title
+                date
+                content(format: RENDERED)
+                tags {
+                    nodes {
+                    name
+                    }
+                }
+                seo {
+                    opengraphUrl
+                    opengraphTitle
+                    opengraphDescription
+                    canonical
+                    opengraphImage {
+                      uri
+                      altText
+                    }
+                  }
+                }
+            }
+        }
+      `;
+
+export const GET_SINGLE_POST_QUERY = `query post($slug: ID!) {
+    post(
+      id: $slug
+      idType: SLUG
+    ) {
+      author {
+        node {
+          name
+        }
+      }
+      categories {
+        nodes {
+          name
+        }
+      }
+      commentCount
+      commentStatus
+      comments {
+        nodes {
+          author {
+            node {
+              name
+              email
+            }
+          }
+          commentId
+          content(format: RAW)
+          date
+          id
+          parentId
+        }
+      }
+      content(format: RENDERED)
+      date
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      id
+      postId
+      tags {
+        nodes {
+          name
+        }
+      }
+      seo {
+          opengraphUrl
+          opengraphTitle
+          opengraphDescription
+          canonical
+          opengraphImage {
+          uri
+          }
+        }
+      title
+    }
+  }`
+
+export const GET_CATEGORY_QUERY = `query getCategory($catName: [String]!){
+          categories(where: {slug: $catName}) {
+          nodes {
+            categoryId
+            name
+            description
+          }
+        }
+      }`;
+
+export const GET_TAG_QUERY = `query getTag($tagName: [String]){
+          tags(where:  {slug: $tagName}) {
+          nodes {
+            description
+            link
+            name
+            slug
+            tagId
+            uri
+          }
+        }
+      }`
+
+export const GET_POST_SITEMAP_QUERY = `
+        query GetPosts($perPage: Int!, $after: String) {
+      posts(first: $perPage, after: $after, where: { status: PUBLISH }) {
+        edges {
+          node {
+            id
+            title
+            slug
+            date
+          }
+          cursor
+        }
+      }
+    }
+      `;
