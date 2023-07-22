6b98b0b1c81f10d086792ed5857ecb5ae6edf656
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-07-22 18:24:26 +0530

    add category canonical

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -6,41 +6,18 @@ import { getTaxonomy } from "@lib/taxonomyParser";
 import { slugify } from "@lib/utils/textConverter";
 import Post from "@partials/Post";
 const { blog_folder } = config.settings;
+import { useRouter } from 'next/router';
+
 
 // category page
-const Category = ({ catDetail, posts, detailPosts }) => {
-	const {name, description} = catDetail;
-	return (
-		<Base title={name}>
-			<div className="section mt-16">
-				<div className="container">
-					<h1 className="h2 mb-12">
-						<span className="section-title ml-1 inline-block capitalize">
-							{name.replace("-", " ")}
-						</span>
-					</h1>
-					<div className="content mb-16">
+const Category = ({ catDetail }) => {
+	const router = useRouter();
 
-					<div
-						className="has-drop-cap-fluid"
-						dangerouslySetInnerHTML={{ __html: description }}
-						></div>
-						</div>
-					<div className="row">
-						<div className="lg:col-8">
-							<div className="row rounded border border-border p-4 px-3 dark:border-darkmode-border lg:p-6">
-								{detailPosts.map((post, i) => (
-									<div key={`key-${i}`} className="col-12 mb-8 sm:col-6">
-										<Post post={post} />
-									</div>
-								))}
-							</div>
-						</div>
-						<Sidebar posts={posts} categories={[]} />
-					</div>
-				</div>
-			</div>
-		</Base>
+	const { uri } = catDetail;
+	router.push(uri); // or router.replace('/destination-page') for a replace redirect
+
+	return (
+		<></>
 	);
 };
 
@@ -63,13 +40,10 @@ export const getStaticPaths = () => {
 export const getStaticProps = async ({ params }) => {
 	const { category } = params;
 	const catDetail = await getCategory(category)
-	const posts = await getAllPosts(category);
-	const detailPosts = await getRelatedPosts(category, '', '', 20);
 
 	return {
 		props: {
 			catDetail: catDetail.nodes[0],
-			posts: posts?.nodes,
-			detailPosts: detailPosts?.nodes},
+		},
 	};
 };
