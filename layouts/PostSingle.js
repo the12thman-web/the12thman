22a084eb552e6ce192ca5640794e3ad543491ef6
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-07-24 22:13:08 +0530

    remove tags for post with no image

--- layouts/PostSingle.js
+++ layouts/PostSingle.js
@@ -71,7 +71,7 @@ const PostSingle = ({
                     />
                   )}
                   <ul className="absolute top-3 left-2 flex flex-wrap items-center">
-                    {tags?.nodes.slice(0,2).map(({name}, index) => (
+                    {featuredImage && tags?.nodes.slice(0,2).map(({name}, index) => (
                       <li
                         className="mx-2 inline-flex h-7 rounded-[35px] bg-primary px-3 text-white"
                         key={"tag-" + index}
