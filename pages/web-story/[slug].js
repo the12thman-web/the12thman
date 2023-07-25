5884959dd3ee8ba8db219eaa8b3b34d443e77ad3
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-07-25 22:23:16 +0530

    fix webstory issue

--- pages/web-story/[slug].js
+++ pages/web-story/[slug].js
@@ -1,42 +1,45 @@
 import { useAmp } from 'next/amp'
 import { getWebStoriesBySlug } from "@lib/graphql";
+import NotFound from '@layouts/404';
 export const config = { amp: true }
 
 const WebStory = ({ content }) => {
 
   const isAmp = useAmp();
   console.log('isAmp', isAmp)
-  return <div>{isAmp && <div
+  return <div>{isAmp && content ? <div
     className="has-drop-cap-fluid"
     dangerouslySetInnerHTML={{ __html: content }}
-  ></div> }
+  ></div> : <NotFound title='Sorry 404! Webstory Not Found' /> }
 </div>
 };
 
 export default WebStory
 
 
-export const getServerSideProps = async (context) => {
-  if (context && context.res) {
-    const { res, params } = context;
-    const slug = params?.slug;
-    // fetch your web story here 
-    const data = await getWebStoriesBySlug(slug);
-    if (!data || !data.nodes || !data.nodes.length) {
-      return {
-        notFound: true,
-      };
-    }
+// category page routes
+export const getStaticPaths = () => {
+  // const allCategories = getTaxonomy(`content/${blog_folder}`, "categories");
+  // const allCategories = [];
+  // const paths = allCategories.map((category) => ({
+  // 	params: {
+  // 		category: category,
+  // 	},
+  // }));
 
-    return {
-      props: {
-        content: data?.nodes[1].content
-      },
-    };
-  }
+  return { paths: [], fallback: 'blocking' };
+};
 
+// category page data
+export const getStaticProps = async ({ params }) => {
+  const slug = params?.slug;
+  // fetch your web story here 
+  const data = await getWebStoriesBySlug(slug);
+  
   return {
     props: {
+      content: data?.content
     },
   };
 };
+
