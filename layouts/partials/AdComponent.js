dd839c38768a480934fba02c0616a9ec821a2bcb
AkankshaGarg22 <garg.akanksha002@gmail.com>
2023-09-06 18:36:49 +0530

    adding width in parent for adsense ads

--- layouts/partials/AdComponent.js
+++ layouts/partials/AdComponent.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export const MyAdComponent = ({client, slot}) => {
+export const MyAdComponent = ({ client, slot }) => {
   useEffect(() => {
     if (window) {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -8,11 +8,14 @@ export const MyAdComponent = ({client, slot}) => {
   }, []);
 
   return (
-    <ins className="adsbygoogle"
-         style={{ display: 'block' }}
-         data-ad-client={client}
-         data-ad-slot={slot}
-         data-ad-format="auto">
-    </ins>
+    <div className='adsense-div' style={{width: '100%'}}>
+      <ins className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client={client}
+        data-ad-slot={slot}
+        data-ad-format="auto"
+        data-full-width-responsive="true">
+      </ins>
+    </div>
   );
 };
