61a6198889762dbb46e4e80b4186a9e5f607e6e8
dharm <singhdharm1008@gmail.com>
2023-11-28 15:00:44 +0530

    issue:twitter widget

--- layouts/PostSingle.js
+++ layouts/PostSingle.js
@@ -27,18 +27,13 @@ import readingTime from "@lib/utils/readingTime";
 import useWindow from "../hooks/useWindow";
 import { MyAdComponent } from "./partials/AdComponent";
 import { UniBotsPlayerComponent } from "./partials/UniBotsPlayer";
+import { useEffect, useRef } from "react";
 
 const { disqus } = config;
 const { meta_author } = config.metadata;
 
-const PostSingle = ({
-  frontmatter,
-  content,
-  slug,
-  trendingPosts
-}) => {
-
-  const isMobile = useWindow(767) < 768
+const PostSingle = ({ frontmatter, content, slug, trendingPosts }) => {
+  const isMobile = useWindow(767) < 768;
 
   let { tags, description, title, date, featuredImage, categories, metaKeywords } = frontmatter;
   description = description ? description : content?.slice(0, 120);
@@ -47,87 +42,88 @@ const PostSingle = ({
   const author = frontmatter.author?.node?.name ? frontmatter.author?.node?.name : meta_author;
   // Local copy so we don't modify global config.
   let disqusConfig = config.disqus.settings;
-  disqusConfig.identifier = frontmatter.disqusId
-    ? frontmatter.disqusId
-    : config.settings.blog_folder + "/" + slug;
+  disqusConfig.identifier = frontmatter.disqusId ? frontmatter.disqusId : config.settings.blog_folder + '/' + slug;
 
   const router = useRouter();
 
   // content = content.replaceAll('img', 'Image');
-  const article_schema = { 
-    "@context": "https://schema.org", 
-    "@type": "NewsArticle", 
-    "mainEntityOfPage": { 
-      "@type": "WebPage", 
-      "@id": BASE_URL + router.asPath 
-    }, 
-    "headline": title, 
-    "image": [featuredImage], 
-    "datePublished": date, 
-    "dateModified": date, 
-    "author": { 
-      "@type": "Person", 
-      "name": author 
-    }, 
-    "publisher": { 
-      "@type": "Organization", 
-      "name": "The 12th Man", 
-      "logo": { 
-      "@type": "ImageObject", 
-      "url": "https://the12thman.in/_next/image?url=%2Fimages%2Flogo%202.png&w=640&q=75", 
-      "height": 60 
-      } 
-    }, 
-    "description": description 
-    } 
+  const article_schema = {
+    '@context': 'https://schema.org',
+    '@type': 'NewsArticle',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': BASE_URL + router.asPath,
+    },
+    headline: title,
+    image: [featuredImage],
+    datePublished: date,
+    dateModified: date,
+    author: {
+      '@type': 'Person',
+      name: author,
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'The 12th Man',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://the12thman.in/_next/image?url=%2Fimages%2Flogo%202.png&w=640&q=75',
+        height: 60,
+      },
+    },
+    description: description,
+  };
 
-  console.log({content, featuredImage});
+  const contentRef = useRef();
+
+  useEffect(() => {
+    // Replace 'img' with 'Image' in the content
+    const updatedContent = content.replaceAll('img', 'Image');
+    contentRef.current.innerHTML = updatedContent;
+
+    // Load Twitter widget script
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [content]);
+
+  console.log({ content, featuredImage });
   return (
     <Base title={title} description={description} metaKeywords={metaKeywords} schema={article_schema}>
-      <section className="py-16 single-blog mt-1 pt-1">
+      <section className="single-blog mt-1 py-16 pt-1">
         <div className="container">
           <div className="row">
             <div className="lg:col-8">
               <article>
-                <div className="relative">
-                  {/* {featuredImage && (
-                    <Image
-                      src={featuredImage}
-                      height="500"
-                      width="1000"
-                      alt={title}
-                      className="rounded-lg"
-                      priority={true}
-                    />
-                  )} */}
-                  <ul className="absolute top-3 left-2 flex flex-wrap items-center">
-                    {/* {featuredImage && tags?.nodes.slice(0,2).map(({name}, index) => (
-                      <li
-                        className="mx-2 inline-flex h-7 rounded-[35px] bg-primary px-3 text-white"
-                        key={"tag-" + index}
-                      >
-                        <Link
-                          className="capitalize"
-                          href={`/tag/${name.toLowerCase().replaceAll(" ", "-")}`}
-                        >
-                          {name}
-                        </Link>
-                      </li>
-                    ))} */}
+                {/* <div className="relative">
+                  {featuredImage && <Image src={featuredImage} height="500" width="1000" alt={title} className="rounded-lg" priority={true} />}
+                  <ul className="absolute left-2 top-3 flex flex-wrap items-center">
+                    {featuredImage &&
+                      tags?.nodes.slice(0, 2).map(({ name }, index) => (
+                        <li className="mx-2 inline-flex h-7 rounded-[35px] bg-primary px-3 text-white" key={'tag-' + index}>
+                          <Link className="capitalize" href={`/tag/${name.toLowerCase().replaceAll(' ', '-')}`}>
+                            {name}
+                          </Link>
+                        </li>
+                      ))}
                   </ul>
-                </div>
+                </div> */}
                 {config.settings.InnerPaginationOptions.enableTop && (
                   <div className="mt-4">
                     <InnerPagination posts={posts} date={date} />
                   </div>
                 )}
-                <div className='totalViews'>
+                <div className="totalViews">
                   <ul className="flex items-center space-x-4">
                     <li>
-                      <Link
-                        className="inline-flex items-center font-secondary text-xs leading-3"
-                        href="/about"
-                      >
+                      <Link className="inline-flex items-center font-secondary text-xs leading-3" href="/about">
                         <FaUserAlt className="mr-1.5 mt-1" />
                         {author}
                       </Link>
@@ -142,7 +138,6 @@ const PostSingle = ({
                     </li>
                   </ul>
                   <div className=" hidden sm:block">
-
                     <ul id="social_link" className="list-inline">
                       <li className="list-inline-item">
                         <FacebookShareButton url={BASE_URL + router.asPath}>
@@ -150,9 +145,7 @@ const PostSingle = ({
                         </FacebookShareButton>
                       </li>
                       <li className="list-inline-item">
-                        <PinterestShareButton
-                          url={BASE_URL + router.asPath}
-                        >
+                        <PinterestShareButton url={BASE_URL + router.asPath}>
                           <PinterestIcon size={32} round />
                         </PinterestShareButton>
                       </li>
@@ -167,7 +160,6 @@ const PostSingle = ({
                         </LinkedinShareButton>
                       </li>
                     </ul>
-
                   </div>
                 </div>
                 <div className="content mb-16">
@@ -175,31 +167,17 @@ const PostSingle = ({
                   {/* {tags?.nodes.slice(0, 2).map(({ name }, index) => (
                     <Tag name={name}/>
                   ))} */}
-                  <div
-                    className="has-drop-cap-fluid"
-                    dangerouslySetInnerHTML={{ __html: content.replaceAll('img', 'Image') }}
-                  ></div>
+                  <div className="has-drop-cap-fluid" ref={contentRef}></div>
                 </div>
-                {config.settings.InnerPaginationOptions.enableBottom && (
-                  <InnerPagination posts={posts} date={date} />
-                )}
+                {config.settings.InnerPaginationOptions.enableBottom && <InnerPagination posts={posts} date={date} />}
               </article>
-              {/* <div className="mt-16">
-                {disqus.enable && (
-                  <DiscussionEmbed
-                    key={theme}
-                    shortname={disqus.shortname}
-                    config={disqusConfig}
-                  />
-                )}
-              </div> */}
+              {/* <div className="mt-16">{disqus.enable && <DiscussionEmbed key={theme} shortname={disqus.shortname} config={disqusConfig} />}</div> */}
             </div>
-            <Sidebar
-              trendingPosts={trendingPosts.filter((post) => post.slug !== slug)}
-              categories={categories}
-            />
+            <Sidebar trendingPosts={trendingPosts.filter(post => post.slug !== slug)} categories={categories} />
             {/** UniBots Player */}
-            <div id="div-ub-the12thman"><UniBotsPlayerComponent></UniBotsPlayerComponent></div>
+            <div id="div-ub-the12thman">
+              <UniBotsPlayerComponent></UniBotsPlayerComponent>
+            </div>
           </div>
         </div>
 
@@ -216,16 +194,14 @@ const PostSingle = ({
         </div> */}
       </section>
       {/* Mobile Share button links */}
-      {<div className="ads-block" style={{ width: '100%', bottom: '65px' }}>
-        {isMobile && <MyAdComponent slot="3700818465" adHeight="50px" adWidth="300px" isMobile={true}></MyAdComponent>}
-      </div>}
+        <div className="ads-block" style={{ width: '100%', bottom: '65px' }}>
+          {isMobile && <MyAdComponent slot="3700818465" adHeight="50px" adWidth="300px" isMobile={true}></MyAdComponent>}
+        </div>
+
       <footer
-        class="bg-neutral-100
-             fixed
-             inset-x-0
-             bottom-0
-             p-4
-             block xl:hidden lg:hidden md:hidden" style={{ zIndex: '500' }}>
+        className="fixed inset-x-0 bottom-0 block bg-neutral-100 p-4 md:hidden lg:hidden xl:hidden"
+        style={{ zIndex: '500' }}
+      >
         <ul id="social_link" className="justify-around">
           <li className="list-inline-item">
             <FacebookShareButton url={BASE_URL + router.asPath}>
@@ -233,9 +209,7 @@ const PostSingle = ({
             </FacebookShareButton>
           </li>
           <li className="list-inline-item">
-            <PinterestShareButton
-              url={BASE_URL + router.asPath}
-            >
+            <PinterestShareButton url={BASE_URL + router.asPath}>
               <PinterestIcon size={32} round />
             </PinterestShareButton>
           </li>
